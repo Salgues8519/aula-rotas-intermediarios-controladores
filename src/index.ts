b43d1702b4bd6c98388e140129f783c88958c8ec
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2024-10-21 15:01:54 -0300

    adicionado um novo caminho  no servidor

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import express from 'express'
 const servidor = express()
 
 servidor.get('/', (req, res):any => {
+    console.log(req);
     return res.send('Servidor está okay!')
+})
+
+servidor.get('/usuarios', (req, res):any => {
+    return res.send('Servidor funcionando, programar é legal')
 })
 
 servidor.listen(process.env.PORT)
