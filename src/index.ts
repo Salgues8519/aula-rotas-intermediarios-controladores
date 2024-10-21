68ace21889e550aa4a49f795a09cc7580e01923f
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2024-10-21 16:15:16 -0300

    adicioado Nodemon ao código

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import express from 'express'
 const servidor = express()
 
 servidor.get('/', (req, res):any => {
-    console.log(req);
-    return res.send('Servidor está okay!')
+    return res.send('Servidor está okay com Nodemon agora!')
 })
 
 servidor.get('/usuarios', (req, res):any => {
     return res.send('Servidor funcionando, programar é legal')
 })
 
-servidor.listen(process.env.PORT)
+servidor.listen(process.env.PORT)   
