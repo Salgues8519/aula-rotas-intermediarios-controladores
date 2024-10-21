d0a5eaee85774074db82996c41f1b433191689c4
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2024-10-21 18:28:24 -0300

    implementado parâmentros de rotas

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,30 @@ import  'dotenv/config'
 import express from 'express'
 const servidor = express()
 
-servidor.get('/', (req, res):any => {
-    return res.send('Servidor está okay com Nodemon agora!')
+
+const pessoas = [
+    {nome: 'rafael', email:'rafael@email.com'},
+    {nome: 'joão', email:'joao@email.com'},
+    {nome: 'maria', email:'maria@email.com'},
+]
+
+servidor.get('/usuario/:nome', (req, res):any => {
+    const { nome } = req.params
+    const pessoa = pessoas.find(item => {
+        return item.nome === nome
+    })
+    
+    if (!pessoa) {
+        return res.send('pessoa não encontrada')
+    }
+
+    return res.send(pessoa)
 })
 
-servidor.get('/usuarios', (req, res):any => {
+
+servidor.get('/produtos/:frutas', (req, res):any => {
+    console.log(req.params.frutas);
+    
     return res.send('Servidor funcionando, programar é legal')
 })
 
